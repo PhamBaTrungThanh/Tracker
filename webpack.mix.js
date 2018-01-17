a991d69513819ec9c870d2141a4d50a18dcc8e76
Trung Thành <phambatrungthanh@gmail.com>
2018-01-17 11:14:00 +0700

    prepare to move to stage 3

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,27 +1,5 @@
 let mix = require('laravel-mix');
 
-mix.webpackConfig({
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules\/(?!(dom7|swiper)\/).*|bower_components)/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: Config.babel()
-                    }
-                ]
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-         // handsontable: path.resolve(__dirname, 'node_modules/handsontable-pro')
-        }
-      },
-});
-
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .extract(['vue', 'axios', 'vue-router', 'vuex',
@@ -30,9 +8,5 @@ mix.js('resources/assets/js/app.js', 'public/js')
              'vue-cleave-component', 'vue-monthly-picker', 'vue-top-progress', 'chart.js']);
     
 mix.sass('resources/assets/sass/app.scss', 'public/css');
-mix.sourceMaps();
-mix.version();
-mix.browserSync({
-    proxy: "tracker.dev",
-    open: false,
-});
+
+mix.browserSync('tracker.dev');
